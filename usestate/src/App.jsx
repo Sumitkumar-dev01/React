eb4bcb1d9d20260c7e62f990cde66c5fc81a525f
sumitkumar2299 <sumitofficialme@gmail.com>
2025-02-14 12:48:31 +0530

    learning react with vishwa sir

---
+++ usestate/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  const [state,setState] = useState(5)
+
+  
+
+  return (
+    
+
+    <>
+    
+    <h1>hello react </h1>
+    adding one year {state}
+    <br />
+    <button onClick={ ()=>{setState(state+1)}}>Add one year</button>
+
+
+  
+    </>
+  )
+}
+
+export default App
